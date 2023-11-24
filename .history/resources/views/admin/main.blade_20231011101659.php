a7a018e5d98ce6f225249f8822883cb7dbfc06e3
teo <phantruongthuatt@gmail.com>
2023-11-24 23:02:58 +0700

    registeradmin

--- .history/resources/views/admin/main.blade_20231011101659.php
+++
@@ -1,3 +0,0 @@
-<h1>
-    <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
-</h1>
--- .history/routes/web_20231011085611.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get ('admin/users/login', 'Admin\Users\LoginController@index')->name('admin.users.login');
